780caeb7cc75fa3746d2217aa559580c37243c52
DHEERAJ KUSHWAHA <165313867+Dhiraj902@users.noreply.github.com>
2025-12-01 21:08:14 +0530

    Add arithmetic and logical operations in String_First_Task.py
    This script performs various arithmetic and logical operations using predefined variables and prints the results.

---
+++ String_First_Task.py
@@ -0,0 +1,103 @@
+a = 15
+b = 4
+c = 9
+d = 20
+nums = [4, 9, 15, 25]
+
+# 1.( a+b)
+a = 15
+b = 4
+print(a + b)
+Output: 19
+
+#2. (d - c)
+d = 20
+c = 9
+print(d - c)
+Output: 11
+
+#3. (b * 2)
+b = 4
+print(b * 2)
+Output: 8
+
+#4. (a % b)
+a = 15
+b = 4
+print(a % b)
+Output: 2
+
+#5. (c > b)
+c = 9
+b = 4
+print(c > b)
+Output: True
+
+# 6. ((a * b) - c)
+a = 15
+b = 4
+c = 9
+print((a * b) - c)
+Output: 51
+
+#7. (d // b)
+d = 20
+b = 4
+print(d // b)
+Output: 5
+
+# 8. ((a > c) and (b < d))
+a = 15
+c = 9
+b = 4
+d = 20
+print((a > c) and (b < d))
+Output: True
+
+#9. a in nums
+a = 15
+nums = [4, 9, 15, 25]
+print(a in nums)
+Output: True
+
+#10. (c ** 2)
+c = 9
+print(c ** 2)
+Output: 81
+
+
+#11. ((a + c) // b) + (d % a))
+a = 15
+c = 9
+b = 4
+d = 20
+print(((a + c) // b) + (d % a))
+Output: 11
+
+#12. ((a ** b) // c)
+a = 15
+b = 4
+c = 9
+print((a ** b) // c)
+Output: 5625
+
+#13. ((a < d) or (c == b))
+a = 15
+d = 20
+c = 9
+b = 4
+print((a < d) or (c == b))
+Output: True
+
+#14. nums is nums
+nums = [4, 9, 15, 25]
+print(nums is nums)
+Output: True
+
+#15.(((d // b) ** 2) - (a % c))
+d = 20
+b = 4
+a = 15
+c = 9
+print(((d // b) ** 2) - (a % c))
+Output: 19
